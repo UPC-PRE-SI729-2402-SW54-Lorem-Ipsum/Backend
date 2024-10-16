807a23b711c571471be6fea32af3c344cbe3983b
Charlie <u201923994@upc.edu.pe>
2024-10-15 19:46:33 -0500

    Primer commit

---
+++ src/main/java/com/loremipsum/lawconnectplatform/LawConnectPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.loremipsum.lawconnectplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawConnectPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LawConnectPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/loremipsum/lawconnectplatform/LawConnectPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loremipsum.lawconnectplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawConnectPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

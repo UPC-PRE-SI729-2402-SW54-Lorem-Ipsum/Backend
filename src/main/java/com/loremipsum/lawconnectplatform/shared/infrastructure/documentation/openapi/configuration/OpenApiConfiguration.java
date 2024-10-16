c4b0e72496b74513614369e21afd1766b7f21088
Charlie <u201923994@upc.edu.pe>
2024-10-15 20:06:02 -0500

    first commit

---
+++ src/main/java/com/loremipsum/lawconnectplatform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -0,0 +1,27 @@
+package com.loremipsum.lawconnectplatform.shared.infrastructure.documentation.openapi.configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfiguration {
+    @Bean
+    public OpenAPI learningPlatformOpenApi() {
+        // General configuration
+        var openApi = new OpenAPI();
+        openApi.info(new Info()
+                        .title("Medic Defense Platform API")
+                        .description("Medic defense Platform application REST API documentation.")
+                        .version("v1.0.0")
+                        .license(new License().name("Apache 2.0")
+                                .url("https://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Medic defense Platform wiki Documentation")
+                        .url("https://github.com/Open-Source-Grupo-Testigos-de-Jehova/backend-Medic-Defense.git"));
+        return openApi;
+    }
+}

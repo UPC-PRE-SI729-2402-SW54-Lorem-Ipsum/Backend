91e100367ffc39bb8b5bd3230b97b0589256839d
CamilaLoli <u202110385@upc.edu.pe>
2024-10-22 21:08:37 -0500

    feat(consultation): added Consultation queries and service.

---
+++ src/main/java/com/loremipsum/lawconnectplatform/consultation/domain/model/queries/GetAllConsultationsByLawyerIdQuery.java
@@ -0,0 +1,4 @@
+package com.loremipsum.lawconnectplatform.consultation.domain.model.queries;
+
+public record GetAllConsultationsByLawyerIdQuery(Long lawyerId) {
+}
---
+++ src/main/java/com/loremipsum/lawconnectplatform/consultation/domain/services/ConsultationQueryService.java
@@ -0,0 +1,4 @@
+package com.loremipsum.lawconnectplatform.consultation.domain.services;
+
+public class ConsultationQueryService {
+}

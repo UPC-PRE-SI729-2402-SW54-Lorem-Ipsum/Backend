5f50249391badd4bea8ade589190b036a059e972
CamilaLoli <u202110385@upc.edu.pe>
2024-10-31 23:08:40 -0500

    fixed(consultation): fixed repository by adding new methods.

--- src/main/java/com/loremipsum/lawconnectplatform/consultation/infrastructure/persistence/jpa/repositories/ConsultationRepository.java
+++ src/main/java/com/loremipsum/lawconnectplatform/consultation/infrastructure/persistence/jpa/repositories/ConsultationRepository.java
@@ -1,11 +1,17 @@
 package com.loremipsum.lawconnectplatform.consultation.infrastructure.persistence.jpa.repositories;
 
 import com.loremipsum.lawconnectplatform.consultation.domain.model.aggregates.Consultation;
+import com.loremipsum.lawconnectplatform.consultation.domain.model.valueobjects.LawyerC;
+import com.loremipsum.lawconnectplatform.consultation.domain.model.valueobjects.PaymentC;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ConsultationRepository extends JpaRepository<Consultation, Long> {
-    boolean existsByClientIdAndLawyerId(Long clientId, Long lawyerId);
-    List<Consultation> findAllByLawyerId(Long lawyerId);
+    boolean existsByPaymentIdAndLawyerId(PaymentC paymentId, LawyerC lawyerId);
+    List<Consultation> findAllByLawyerId(LawyerC lawyerId);
+    List<Consultation> findAllByPaymentId(PaymentC paymentId);
+    List<Consultation> findAllByPaymentIdAndLawyerId(PaymentC paymentId, LawyerC lawyerId);
 }

18c459b36c71f31e89f17402e15cd204b2d7da80
CamilaLoli <u202110385@upc.edu.pe>
2024-10-31 23:00:44 -0500

    feat(consultation): added DeleteConsultationCommand in service.

--- src/main/java/com/loremipsum/lawconnectplatform/consultation/domain/services/ConsultationCommandService.java
+++ src/main/java/com/loremipsum/lawconnectplatform/consultation/domain/services/ConsultationCommandService.java
@@ -1,7 +1,9 @@
 package com.loremipsum.lawconnectplatform.consultation.domain.services;
 
 import com.loremipsum.lawconnectplatform.consultation.domain.model.commands.CreateConsultationCommand;
+import com.loremipsum.lawconnectplatform.consultation.domain.model.commands.DeleteConsultationCommand;
 
 public interface ConsultationCommandService {
     Long handle(CreateConsultationCommand command);
+    void handle(DeleteConsultationCommand command);
 }

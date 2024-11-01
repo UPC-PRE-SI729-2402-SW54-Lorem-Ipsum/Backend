b36978a53a2632b840c4e9a241b5b9c1abe58f4c
CamilaLoli <u202110385@upc.edu.pe>
2024-10-31 23:04:12 -0500

    feat(consultation): added new queries in Consultation Query Service.

--- src/main/java/com/loremipsum/lawconnectplatform/consultation/domain/model/queries/GetAllConsultationsByPaymentIdQuery.java
+++ src/main/java/com/loremipsum/lawconnectplatform/consultation/domain/model/queries/GetAllConsultationsByPaymentIdQuery.java
@@ -2,5 +2,5 @@ package com.loremipsum.lawconnectplatform.consultation.domain.model.queries;
 
 import com.loremipsum.lawconnectplatform.consultation.domain.model.valueobjects.PaymentC;
 
-public record GetAllConsultationsByPaymentId(PaymentC paymentId) {
+public record GetAllConsultationsByPaymentIdQuery(PaymentC paymentId) {
 }

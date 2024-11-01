93657f66d91ab494d3bfafb9ff4c7256471d8e84
CamilaLoli <u202110385@upc.edu.pe>
2024-10-31 22:57:06 -0500

    fix(consultation): deleted ClientC since it's redundant.

--- src/main/java/com/loremipsum/lawconnectplatform/consultation/domain/model/aggregates/Consultation.java
+++ src/main/java/com/loremipsum/lawconnectplatform/consultation/domain/model/aggregates/Consultation.java
@@ -3,7 +3,6 @@ package com.loremipsum.lawconnectplatform.consultation.domain.model.aggregates;
 import com.loremipsum.lawconnectplatform.consultation.domain.model.commands.CreateConsultationCommand;
 import com.loremipsum.lawconnectplatform.consultation.domain.model.valueobjects.ConsultationType;
 import com.loremipsum.lawconnectplatform.consultation.domain.model.valueobjects.LawyerC;
-import com.loremipsum.lawconnectplatform.consultation.domain.model.valueobjects.ClientC;
 import com.loremipsum.lawconnectplatform.consultation.domain.model.valueobjects.PaymentC;
 import com.loremipsum.lawconnectplatform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
 import jakarta.persistence.Column;
@@ -21,9 +20,6 @@ public class Consultation extends AuditableAbstractAggregateRoot<Consultation> {
     private LawyerC lawyerId;
 
     @Embedded
-    private ClientC clientId;
-
-    @Embedded
     private PaymentC paymentId;
 
     @Column(nullable = false)
@@ -35,16 +31,14 @@ public class Consultation extends AuditableAbstractAggregateRoot<Consultation> {
 
     public Consultation() {
         this.lawyerId = new LawyerC(null);
-        this.clientId = new ClientC(null);
         this.paymentId = new PaymentC(null);
         this.consultationType = ConsultationType.NONE;
         this.description = "";
     }
 
-    public Consultation(Long lawyerId, Long clientId, Long paymentId, String consultationType, String description){
+    public Consultation(Long lawyerId, Long paymentId, String consultationType, String description){
         this();
         this.lawyerId = new LawyerC(lawyerId);
-        this.clientId = new ClientC(clientId);
         this.paymentId = new PaymentC(paymentId);
         this.consultationType = ConsultationType.valueOf(consultationType);
         this.description = description;
@@ -53,7 +47,6 @@ public class Consultation extends AuditableAbstractAggregateRoot<Consultation> {
     public Consultation(CreateConsultationCommand command){
         this();
         this.lawyerId = new LawyerC(command.lawyerId());
-        this.clientId = new ClientC(command.clientId());
         this.paymentId = new PaymentC(command.paymentId());
         this.consultationType = ConsultationType.valueOf(command.consultationType());
         this.description = command.description();
@@ -61,10 +54,6 @@ public class Consultation extends AuditableAbstractAggregateRoot<Consultation> {
 
     public Long getLawyerId() {
         return this.lawyerId.lawyerId();
-    }
-
-    public Long getClientId() {
-        return this.clientId.clientId();
     }
 
     public Long getPaymentId() {
--- src/main/java/com/loremipsum/lawconnectplatform/consultation/domain/model/valueobjects/ClientC.java
+++
@@ -1,7 +0,0 @@
-package com.loremipsum.lawconnectplatform.consultation.domain.model.valueobjects;
-
-import jakarta.persistence.Embeddable;
-
-@Embeddable
-public record ClientC(Long clientId) {
-}

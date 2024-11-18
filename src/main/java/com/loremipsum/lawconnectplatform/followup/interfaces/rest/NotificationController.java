6550c1c552cc6ec029381a39baffebb360cb8c89
CamilaLoli <u202110385@upc.edu.pe>
2024-11-18 16:55:58 -0500

    feat(notification): added a summary for each endpoint.

--- src/main/java/com/loremipsum/lawconnectplatform/followup/interfaces/rest/NotificationController.java
+++ src/main/java/com/loremipsum/lawconnectplatform/followup/interfaces/rest/NotificationController.java
@@ -10,6 +10,7 @@ import com.loremipsum.lawconnectplatform.followup.interfaces.rest.resources.Crea
 import com.loremipsum.lawconnectplatform.followup.interfaces.rest.resources.NotificationResource;
 import com.loremipsum.lawconnectplatform.followup.interfaces.rest.transform.CreateNotificationCommandFromResourceAssembler;
 import com.loremipsum.lawconnectplatform.followup.interfaces.rest.transform.NotificationResourceFromEntityAssembler;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -31,28 +32,31 @@ public class NotificationController {
         this.notificationQueryService = notificationQueryService;
     }
 
+    @Operation(summary = "Create A Notification")
     @PostMapping
     public ResponseEntity<NotificationResource> createNotification(@RequestBody CreateNotificationResource createNotificationResource) {
         var createNotificationCommand = CreateNotificationCommandFromResourceAssembler.toCommandFromResource(createNotificationResource);
         var notification = notificationCommandService.handle(createNotificationCommand);
-        if(notification.isEmpty()) {
+        if (notification.isEmpty()) {
             return ResponseEntity.badRequest().build();
         }
         var notificationResource = NotificationResourceFromEntityAssembler.toResourceFromEntity(notification.get());
         return new ResponseEntity<>(notificationResource, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Get A Notification By Id")
     @GetMapping("/{notificationId}")
     public ResponseEntity<NotificationResource> getNotificationById(@PathVariable Long notificationId) {
         var getNotificationByIdQuery = new GetNotificationByIdQuery(notificationId);
         var notification = notificationQueryService.handle(getNotificationByIdQuery);
-        if(notification.isEmpty()) {
+        if (notification.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
         var notificationResource = NotificationResourceFromEntityAssembler.toResourceFromEntity(notification.get());
         return ResponseEntity.ok(notificationResource);
     }
 
+    @Operation(summary = "Get All Notifications By Consultation Id")
     @GetMapping("/legal-case/{consultationId}")
     public ResponseEntity<List<NotificationResource>> getAllNotificationsByConsultationId(@PathVariable Long consultationId) {
         var notifications = notificationQueryService.handle(new GetAllNotificationByConsultationIdQuery(consultationId));
@@ -62,7 +66,7 @@ public class NotificationController {
         return ResponseEntity.ok(notificationsResources);
     }
 
-
+    @Operation(summary = "Get All Notifications By Client Id")
     @GetMapping("/client/{clientId}")
     public ResponseEntity<List<NotificationResource>> getAllNotificationsByClientId(@PathVariable Long clientId) {
         var notifications = notificationQueryService.handle(new GetAllNotificationsByClientIdQuery(clientId));
@@ -72,6 +76,7 @@ public class NotificationController {
         return ResponseEntity.ok(notificationsResources);
     }
 
+    @Operation(summary = "Delete A Notification")
     @DeleteMapping("/{notificationId}")
     public ResponseEntity<?> deleteNotification(@PathVariable Long notificationId) {
         notificationCommandService.handle(new DeleteNotificationCommand(notificationId));

359cc1e9d19e9b6de2f74087259b434347461b9b
CamilaLoli <u202110385@upc.edu.pe>
2024-11-18 15:59:44 -0500

    feat(Follow-up): added restriction of not null.

--- src/main/java/com/loremipsum/lawconnectplatform/followup/domain/model/aggregates/Notification.java
+++ src/main/java/com/loremipsum/lawconnectplatform/followup/domain/model/aggregates/Notification.java
@@ -2,19 +2,28 @@ package com.loremipsum.lawconnectplatform.followup.domain.model.aggregates;
 
 import com.loremipsum.lawconnectplatform.followup.domain.model.commands.CreateNotificationCommand;
 import com.loremipsum.lawconnectplatform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 @Entity
 public class Notification extends AuditableAbstractAggregateRoot<Notification> {
 
+    @NotNull
+    @Size(max = 250)
     private String title;
 
+    @NotNull
+    @Size(max = 500)
     private String description;
 
+    @NotNull
     private Long clientId;
 
+    @NotNull
     private Long consultationId;
 
     public Notification(CreateNotificationCommand command) {
